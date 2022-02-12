421e81cf9f5175c2f56ad6fe2201547b287f12a1
Paul <quazantip@gmail.com>
2022-02-12 22:02:39 +0400

    Created PropertiesUtil;

--- src/main/java/ua/goit/Main.java
+++ src/main/java/ua/goit/Main.java
@@ -1,15 +1,19 @@
 package ua.goit;
 
 import ua.goit.config.DataBaseManagerConnector;
+import ua.goit.config.PropertiesUtil;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-
-        DataBaseManagerConnector connector = new DataBaseManagerConnector("localhost", 5432, "postgres",
-                "postgres", "19902007");
+        PropertiesUtil util = new PropertiesUtil();
+        DataBaseManagerConnector connector = new DataBaseManagerConnector(util.getHostname(),
+                util.getPort(),
+                util.getSchema(),
+                util.getUser(),
+                util.getPassword());
         try {
             Connection connect = connector.getConnection();
         } catch (SQLException e) {
